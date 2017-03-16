044f18c11dedfe9550355361333e4972582cc28a
FireLord1 <firelord.xda@gmail.com>
2017-03-16 15:01:00 +0530

    p1a42: ignore mouse icon
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,5 +86,5 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
 TW_NEW_ION_HEAP := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_SCREEN_BLANK_ON_BOOT := true
-
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
