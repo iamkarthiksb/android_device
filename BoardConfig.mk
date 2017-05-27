0ec9e6ce6900a5d46ef3b593589ca8a82d7a3d8b
FireLord1 <firelord.xda@gmail.com>
2017-05-28 02:57:27 +0530

    kuntao: No need of dtb
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := sched_enable_hmp=1 console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.emmc=true msm_rtb.filter=0x237 androidboot.hwid=TBD androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 TARGET_KERNEL_SOURCE := kernel/lenovo/p1a42
